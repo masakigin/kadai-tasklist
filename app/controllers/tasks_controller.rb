be28c37ab236397600fb0143d6c00fb7c08f155d
masaki.yamamoto <masakigin0703@gmail.com>
2019-01-09 11:38:31 +0000

    syuusei2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,11 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
-  def index
-      @tasks = Task.all
-      # @tasks = current_user.tasks
-  end
+  
 
   def show
-  end
-
-  def new
-      @task = Task.new
   end
 
   def create
